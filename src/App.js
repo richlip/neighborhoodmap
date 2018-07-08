0e674b8d1d2f0dee3d2d6b487226b4f8eebc63a3
richard <richlip@googlemail.com>
2018-07-08 08:49:47 +0200

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map';
+{/*import Sidebar from "./Sidebar";*/}
 
 class App extends Component {
   render() {
---
+++ src/components/Map.js
@@ -0,0 +1,22 @@
+import React, {
+    Component
+} from 'react';
+
+class Map extends Component {
+
+
+    render() {
+
+        return ( <
+            div id = "map-container" >
+            <
+            div id = "map"
+            role = "application" >
+            <
+            /div> <
+            /div>
+        )
+    }
+}
+
+export default Map;
