dd8064cfbaf372df5a8d133c2d1544aad1c88265
richard <richlip@googlemail.com>
2018-07-09 22:54:32 +0200

    basis work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Map from './components/Map';
-import Sidebar from "./Sidebar";
+
 
 class App extends Component {
   render() {
