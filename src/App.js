2182d267a66611657cc243aa5782e3d1fb3e029f
richard <richlip@googlemail.com>
2018-07-09 22:46:05 +0200

    refinement

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
-{/*import Sidebar from "./Sidebar";*/}
+import Sidebar from "./Sidebar";
 
 class App extends Component {
   render() {
