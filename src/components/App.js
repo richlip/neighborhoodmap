957dd3f58cd9038ebdf6684e3a0d05e8e559eae4
richard <richlip@googlemail.com>
2018-07-18 10:52:15 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   componentDidMount() {
     window.initMap = this.initMap;
     loadMapJS(
-      "https://maps.googleapis.com/maps/api/js?key=AAAIzaSyCnx_1AtdIREVDeTT9IsCMp5k9pCA1W17Q&callback=initMap"
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCnx_1AtdIREVDeTT9IsCMp5k9pCA1W17Q&callback=initMap"
     );
   }
 
